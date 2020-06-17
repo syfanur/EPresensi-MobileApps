aea673597574ac53ccaf1407ceebe02b119e0bac
Syfanur <j0h@example.com>
2020-06-17 15:11:46 +0700

    update login dan profil

--- E-Presensi/app/src/main/java/com/example/adhibeton/Login.java
+++ E-Presensi/app/src/main/java/com/example/adhibeton/Login.java
@@ -3,12 +3,49 @@ package com.example.adhibeton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import cyd.awesome.material.AwesomeText;
+import cyd.awesome.material.FontCharacterMaps;
 
 public class Login extends AppCompatActivity {
+
+    EditText npp, password;
+    AwesomeText ImgShowHidePassword;
+
+    boolean pwd_status = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        npp = (EditText) findViewById(R.id.npp);
+        password = (EditText) findViewById(R.id.password);
+
+        ImgShowHidePassword = (AwesomeText)findViewById(R.id.ImgShowPassword);
+
+        ImgShowHidePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pwd_status) {
+                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    pwd_status = false;
+                    ImgShowHidePassword.setMaterialDesignIcon(FontCharacterMaps.MaterialDesign.MD_VISIBILITY);
+                    password.setSelection(password.length());
+                } else {
+                    password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+                    pwd_status = true;
+                    ImgShowHidePassword.setMaterialDesignIcon(FontCharacterMaps.MaterialDesign.MD_VISIBILITY_OFF);
+                    password.setSelection(password.length());
+                }
+            }
+        });
+
+
     }
 }
