26f696b02f292ccd5e62c08a434f4c32ff88c55b
andhikurniawan <andhiikurniawann@gmail.com>
2020-06-26 10:29:04 +0700

    push detectface

--- E-Presensi/app/src/main/java/com/example/adhibeton/HalamanSelfie.java
+++ E-Presensi/app/src/main/java/com/example/adhibeton/HalamanSelfie.java
@@ -2,7 +2,9 @@ package com.example.adhibeton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HalamanSelfie extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HalamanSelfie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_selfie);
+    }
+
+    public void MoveToFaceDetect(View view) {
+        Intent home = new Intent(HalamanSelfie.this, FaceDetect.class);
+        startActivity(home);
     }
 }
