bf82573bae3fddf43b259540a596cb1a22a98e26
Syfanur <j0h@example.com>
2020-07-02 20:48:24 +0700

    tes

--- E-Presensi/app/src/main/java/com/example/adhibeton/IzinProses.java
+++ E-Presensi/app/src/main/java/com/example/adhibeton/IzinProses.java
@@ -30,6 +30,7 @@ public class IzinProses extends Fragment {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
