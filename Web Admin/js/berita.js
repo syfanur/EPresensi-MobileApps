647a618e95e9e13c630df974fe4a3f8576190930
syfanur <syfanur31@gmail.com>
2021-03-25 01:18:34 +0700

    mobile

--- Web Admin/js/berita.js
+++
@@ -1,9 +0,0 @@
-function deleteData(childKey) {
-  // body...
-  var key = document.getElementById(deleteBtn);
-  if (confirm("Are you sure?")) {
-    firebase.database().ref().child('Pengumuman/'+ childKey).remove();
-    alert('Data Berhasil Dihapus');
-    window.location.reload();
-  }
-}
